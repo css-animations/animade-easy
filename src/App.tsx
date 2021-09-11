66944a2663f38cba0e13803d6907f427f1168204
Matthew Nieva <matthewcn56@gmail.com>
2021-09-10 22:45:01 -0700

    set-up cross site

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { devtools } from "webextension-polyfill";
+
+// function spinny() {
+//   console.log(document);
+//   //document.body.style.backgroundColor = "#000000";
+//   const head = document.getElementsByTagName("HEAD")[0];
+//   const newStyle = document.createElement("style");
+//   newStyle.innerHTML = `
+//     @keyframes spinny {
+//       from {
+//         transform: rotate(0deg);
+//       }
+//       to {
+//         transform: rotate(360deg);
+//       }
+//     }
+//     .post {
+//       animation: spinny infinite 20s linear;
+//     }
+//     `;
+//   head.appendChild(newStyle);
+// }
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 function App() {
   const [headContent, setHeadContent] = useState("");
   const [classInput, setClassInput] = useState("");
@@ -11,15 +39,62 @@ function App() {
     setHeadContent(head.innerHTML);
   }, []);
 
-  function injectCSS(chosenClass: string) {
-    const head = window.document.getElementsByTagName("HEAD")[0];
-    const newStyle = document.createElement("style");
-    newStyle.innerHTML = `
+  async function injectCSS(chosenClass: string) {
+    //const head = window.document.getElementsByTagName("HEAD")[0];
+    //const newStyle = document.createElement("style");
+    const tab = await getCurrentTab();
+
+    // function spinnyJS() {
+    //   console.log(document);
+    //   //document.body.style.backgroundColor = "#000000";
+    //   const head = document.getElementsByTagName("HEAD")[0];
+    //   const newStyle = document.createElement("style");
+    //   newStyle.innerHTML = `
+    //     @keyframes spinny {
+    //       from {
+    //         transform: rotate(0deg);
+    //       }
+    //       to {
+    //         transform: rotate(360deg);
+    //       }
+    //     }
+    //     .${chosenClass} {
+    //       animation: spinny infinite 20s linear;
+    //     }
+    //     `;
+    //   console.log(newStyle);
+    //   head.appendChild(newStyle);
+    // }
+    const newCSS = `
+    @keyframes spinny {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
     .${chosenClass} {
-      animation: App-logo-spin infinite 20s linear;
+      animation: spinny infinite 20s linear;
     }
     `;
-    head.appendChild(newStyle);
+    //alert("tab is: " + tab.id);
+    //alert("tab is: " + tab.id);
+    if (tab.id || tab.id === 0) {
+      try {
+        chrome.scripting.insertCSS(
+          {
+            target: { tabId: tab.id },
+            css: newCSS,
+          },
+          () => {
+            alert("Sucessfully inserted CSS!");
+          }
+        );
+      } catch (error) {
+        alert(error);
+      }
+    } else alert("Invalid Tab ID!");
   }
 
   function resetCSS() {
