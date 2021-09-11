0263f731b9770cfee9331042f92b69fdef19240d
Matthew Nieva <matthewcn56@gmail.com>
2021-09-10 19:30:05 -0700

    dev-tool shenanigans

---
+++ src/devtools/devtools.js
@@ -0,0 +1,22 @@
+/**
+This script is run whenever the devtools are open.
+In here, we can create our panel.
+*/
+
+function handleShown() {
+  console.log("panel is being shown");
+}
+
+function handleHidden() {
+  console.log("panel is being hidden");
+}
+
+/**
+Create a panel, and add listeners for panel show/hide events.
+*/
+browser.devtools.panels
+  .create("My Panel", "/public/logo192.png", "build/index.html")
+  .then((newPanel) => {
+    newPanel.onShown.addListener(handleShown);
+    newPanel.onHidden.addListener(handleHidden);
+  });
